88660a595d8b5bdcfaf3af67bcbb8dca3486ea42
camikaze99 <48164146+camikaze99@users.noreply.github.com>
2019-05-31 23:43:48 +0200

    Update retake2.py

--- retake2.py
+++ retake2.py
@@ -7,14 +7,13 @@ import pprint
 # Opens the pcap file and uses the pcap library to turn it into usable data
 rdr = pcap.open_offline('xsupport.pcap')
 
-# Declaring count to count the number of suspicious packets later
-# OLD: count = 0
 connection_info = dict()
 
 """
 This function prints the timestamp, source IP address,
 destination IP address, source port and destination port and uses a format.
 """
+
 def printNoAck(time, sAdd, dAdd, sPo, dPo):
 
     print("\nTimestamp: \t\t\t", time, "\nSource IP Address: \t\t" , sAdd,
@@ -31,7 +30,7 @@ for hdr, data in rdr:
     tcp = pckt.Tcp.decode(ip.pl)
     if tcp is None: continue
 
-    # Key is based on the ports and ips.
+# Key is based on the ports and IP addresses
     key = "{}{}{}{}".format(ip.saddr, ip.daddr, tcp.sport, tcp.dport)
     if not key in connection_info:
         connection_info[key] = dict()
@@ -39,31 +38,27 @@ for hdr, data in rdr:
         connection_info[key]['destination_ip'] = str(ip.daddr)
         connection_info[key]['source_port'] = str(tcp.sport)
         connection_info[key]['destination_port'] = str(tcp.dport)
-        # De rest van de info kan je hier aanvullen :^)
-    # Increase the number of attempted connections.
+
+# Increases the number of attempted connections
     if 'connection_attempts' not in connection_info[key]:
         connection_info[key]['connection_attempts'] = 0
     connection_info[key]['connection_attempts'] += 1
 
-# If the TCP acknowledgement has a value of 0, it means that there was no ack.
+# If the TCP acknowledgement has a value of 0, it means that there was no ack
     if tcp.ack == 0:
         printNoAck(hdr.ts, ip.saddr, ip.daddr, tcp.sport, tcp.dport)
-# The count variable keeps track of the amount of suspicious packets
-        # OLD: count = count + 1
         if 'no_ack' not in connection_info[key]:
             connection_info[key]['no_ack'] = 0
-            # Also keep a list of timestamps for the no acks.
+# Keeps track of a list of timestamps for the no acks
             connection_info[key]['no_ack_times'] = []
-        # Increase the number of no acknowledgments.
+# Increases the number of no acks
         connection_info[key]['no_ack'] += 1
         connection_info[key]['no_ack_times'].append(hdr.ts)
-# Displays the total amount of suspicious packets
-# OLD: print("\nSuspicious packets: \t\t", count)
 
-# Find suspicious connections: 1 or more no ack.
+# Find suspicious connections: 1 or more connections without an ack
 min_no_ack = 1
 susp_info = dict()
-#  Filter the connection information.
+# Filter the connection information
 for key in connection_info:
     single_connection = connection_info[key]
     if 'no_ack' in single_connection and single_connection['no_ack'] >= min_no_ack:
